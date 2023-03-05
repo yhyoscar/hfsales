020a3e2cad48df685dad63ff77cdac93c3724157
Haiyang Yu <yuhaiyang@google.com>
2023-03-04 23:19:21 -0500

    complete customer model

--- src/hfweb/customer/models.py
+++ src/hfweb/customer/models.py
@@ -9,15 +9,34 @@ class CustomerGroup(models.Model):
     def __str__(self):
         return self.name
 
-    def get_customer_count():
+    def get_customer_count(self):
         return Customer.objects.filter(group__id=self.id).distinct().count()
 
-    def get_member_count():
+    def get_member_count(self):
         return Customer.objects.filter(group__id=self.id, is_member=True).distinct().count()
+
+
+class Address(models.Model):
+    street_address   = models.CharField(max_length=50)
+    street_address_2 = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2, default="NY")
+    zip = models.CharField(max_length=5)
+    distance_from_farm = models.FloatField(verbose_name="到禾富农场的距离（单位：mile）", blank=True, null=True)
+    group = models.CharField(max_length=50, verbose_name="地址分组", blank=True, null=True)
 
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
-    is_member = models.BooleanField(default=False, verbose_name="Is a member of Hephzibah farm?")
-    membership_start_time = models.DateTimeField(blank=True, null=True)
-     
+    phone = models.CharField(max_length=10)
+    group = models.ForeignKey('CustomerGroup', on_delete=models.CASCADE, null=True, blank=True)
+    address = models.ForeignKey('Address', on_delete=models.CASCADE, null=True, blank=True)
+    pickup_address = models.ForeignKey('Address', on_delete=models.CASCADE, null=True, blank=True)
+    email = models.CharField(max_length=50, null=True, blank=True)
+    wechat = models.CharField(max_length=50, null=True, blank=True)
+    facebook = models.CharField(max_length=10, null=True, blank=True)
+    is_member = models.BooleanField(default=False, verbose_name="是否为禾富会员？")
+    membership_start_time = models.DateTimeField(blank=True, null=True, verbose_name="注册会员时间")
+    balance = models.FloatField(blank=True, null=True, verbose_name="账户余额")
+
+
