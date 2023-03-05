543b0d14710b948390af90194ea2bd21065cd4ee
Haiyang Yu <yuhaiyang@google.com>
2023-03-04 23:41:13 -0500

    update admin

--- src/hfweb/customer/admin.py
+++ src/hfweb/customer/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomerGroup, Address, Customer
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
+@admin.register(CustomerGroup)
+class CustomerGroupAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    fields = ['name']
+    list_filter = ()
+    readonly_fields = ()
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['street_address', 'city', 'state', 'zip', 'distance_from_farm', 'group']
+    fields = [('street_address', 'street_address_2', 'city', 'state', 'zip'), 'distance_from_farm', 'group']
+    list_filter = ('city', 'state', 'group')
+    readonly_fields = ()
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email', 'is_member', 'balance', 'group']
+    fields = ['name', 'phone', 'group', 'address', 'pickup_address', 'email', 'wechat', 'facebook', 'is_member', 
+        'membership_start_time', 'balance']
+    list_filter = ('group', 'pickup_address', 'is_member')
+    readonly_fields = ()
+
+
--- src/hfweb/customer/models.py
+++ src/hfweb/customer/models.py
@@ -18,12 +18,18 @@ class CustomerGroup(models.Model):
 
 class Address(models.Model):
     street_address   = models.CharField(max_length=50)
-    street_address_2 = models.CharField(max_length=50, blank=True, null=True)
+    street_address_2 = models.CharField(max_length=50, blank=True, null=True, default="")
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2, default="NY")
     zip = models.CharField(max_length=5)
     distance_from_farm = models.FloatField(verbose_name="到禾富农场的距离（单位：mile）", blank=True, null=True)
     group = models.CharField(max_length=50, verbose_name="地址分组", blank=True, null=True)
+
+    def __str__(self):
+        if self.street_address_2 == "":
+            return f"{self.street_address}, {self.city}, {self.state} {self.zip}"
+        else:
+            return f"{self.street_address}, {self.street_address_2}, {self.city}, {self.state} {self.zip}"
 
 
 class Customer(models.Model):
@@ -39,5 +45,6 @@ class Customer(models.Model):
     membership_start_time = models.DateTimeField(blank=True, null=True, verbose_name="注册会员时间")
     balance = models.FloatField(blank=True, null=True, verbose_name="账户余额")
 
-
+    def __str__(self):
+        return self.name
 
