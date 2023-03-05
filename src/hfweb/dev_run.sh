0b3ce36038bf5e4139b4750e5438598792220f59
oscar <yhyoscar@gmail.com>
2023-03-05 03:44:45 +0000

    start with a skeleton

---
+++ src/hfweb/dev_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf */migrations
+rm -rf */__pycache__
+rm -rf */*/__pycache__
+rm -rf */*/*/__pycache__
+
+
+#python3 manage.py makemigrations customer
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+python3 manage.py collectstatic
+#python3 manage.py runserver 127.0.0.1:7000
+python3 manage.py runserver 0.0.0.0:7000
+
