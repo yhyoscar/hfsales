19a636a032459c26a5419f8c2d32752304e92c5a
oscar <yhyoscar@gmail.com>
2023-03-05 04:02:19 +0000

    add models

--- src/hfweb/customer/models.py
+++ src/hfweb/customer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CustomerGroup(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_customer_count():
+        return Customer.objects.filter(group__id=self.id).distinct().count()
+
+    def get_member_count():
+        return Customer.objects.filter(group__id=self.id, is_member=True).distinct().count()
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    is_member = models.BooleanField(default=False, verbose_name="Is a member of Hephzibah farm?")
+    membership_start_time = models.DateTimeField(blank=True, null=True)
+     
