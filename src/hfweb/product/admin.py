44f4f0c70f59213184c1e0ca7e2c53004dec43e5
oscar <yhyoscar@gmail.com>
2023-03-09 01:09:12 +0000

    update product model

--- src/hfweb/product/admin.py
+++ src/hfweb/product/admin.py
@@ -6,7 +6,10 @@ from .models import Product
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'show_storage', 'need_refrig', 'need_freeze']
-    fields = ['name', 'category', 'picture', 'description', 'unit', 'unit_price', 'number_in_stock',
-        'need_refrig', 'need_freeze']
+    fields = ['name', 'category', 'picture', 'show_picture', 'description', 
+            'unit', 'unit_price', 'number_in_stock',
+            'need_refrig', 'need_freeze']
     list_filter = ('category', 'need_refrig', 'need_freeze')
-    readonly_fields = ('show_picture', )
+    readonly_fields = ("show_picture", )
+
+
--- src/hfweb/product/models.py
+++ src/hfweb/product/models.py
@@ -6,7 +6,7 @@ from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name="产品名称")
+    name = models.CharField(max_length=100, verbose_name="产品名称")
     category = models.CharField(max_length=2, verbose_name="产品类别",
         choices=[('MR', '食用菌'),
                  ('VG', '蔬菜'),
@@ -20,12 +20,14 @@ class Product(models.Model):
     unit = models.CharField(max_length=2, verbose_name="单位", null=True, blank=True,
         choices=[('G', '个'),
                  ('Z', '只'),
-                 #('H', '半只'),
-                 ('12', '打（12个）'),
+                 ('H', '份(每份半只)'),
+                 ('B', '袋(每袋50个)'),
+                 ('12', '打(每打12个)'),
                  ('lb', '磅'),
                  ('oz', '盎司')])
-    unit_price = models.FloatField(verbose_name="单价", null=True, blank=True)
-    number_in_stock = models.FloatField(verbose_name="现有存货数量", null=True, blank=True)
+    #unit_note = models.CharField(max_length=20, verbose_name="单位备注", null=True, blank=True)
+    unit_price = models.FloatField(verbose_name="单价($)", null=True, blank=True)
+    number_in_stock = models.FloatField(verbose_name="库存数量", null=True, blank=True)
     need_refrig = models.BooleanField(default=False, verbose_name="需要冷藏")
     need_freeze = models.BooleanField(default=False, verbose_name="需要冷冻")
 
@@ -33,10 +35,18 @@ class Product(models.Model):
         return self.name
 
     def show_storage(self):
-        return f"{self.number_in_stock} {self.get_unit_display()}"
-    show_storage.short_description = "存货数量"
+        if self.number_in_stock > 0:
+            out = f'<b><font color="green">{self.number_in_stock}</font></b>'
+        else:
+            out = f'<b><font color="red">{self.number_in_stock}</font></b>'
+        out = f"{out} {self.get_unit_display()}"
+        if hasattr(self, "unit_note"):
+            if self.unit_note:
+                out += f"({self.unit_note})"
+        return mark_safe(out)
+    show_storage.short_description = "库存数量"
 
     def show_picture(self):
-        return mark_safe(u'<img src="%s" />' % escape(self.picture.url))
+        return mark_safe(u'<img src="%s" ' % escape(self.picture.url) + 'style="max-width: 100%;">')
     show_picture.short_description = '产品图片'
     show_picture.allow_tags = True    
